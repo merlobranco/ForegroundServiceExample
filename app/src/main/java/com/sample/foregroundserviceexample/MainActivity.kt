7cbbd712286f1de23d77c542d53ec83db3438d8e
Brais Cidras <braisomary@gmail.com>
2020-08-24 10:27:15 +0200

    Adding graphical controls for testing purposes

--- app/src/main/java/com/sample/foregroundserviceexample/MainActivity.kt
+++ app/src/main/java/com/sample/foregroundserviceexample/MainActivity.kt
@@ -2,11 +2,20 @@ package com.sample.foregroundserviceexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun startService(view: View) {
+
+    }
+
+    fun stopService(view: View) {
+
     }
 }
