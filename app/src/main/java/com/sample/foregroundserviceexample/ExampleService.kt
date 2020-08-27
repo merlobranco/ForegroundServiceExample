93e042967d468c33a4e92ced3de60de3e84dbdcb
Brais Cidras <braisomary@gmail.com>
2020-08-27 08:08:33 +0200

    Adding Foreground Service Useful Notes

--- app/src/main/java/com/sample/foregroundserviceexample/ExampleService.kt
+++ app/src/main/java/com/sample/foregroundserviceexample/ExampleService.kt
@@ -14,6 +14,12 @@ class ExampleService: Service() {
         super.onCreate()
     }
 
+    /**
+     * The work we do in onStartCommand runs on UI thread by default
+     * Which means if we do some heavy operations in here without creating a separated working thread (coroutine)
+     * and then start our App, the App will not respond
+     * There is a Subclass of Service that automatically creates a worker thread called IntentService
+     */
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         val input = intent!!.getStringExtra(INPUT_EXTRA_KEY)
 
@@ -31,7 +37,7 @@ class ExampleService: Service() {
 
         // Since we are using a service we could call the this method
         // instead of notificationManager.notify(1, notification)
-        startForeground(1, notification)
+//        startForeground(1, notification)
 
         /**
          * We will return what will happen when our system kills our service
@@ -40,6 +46,12 @@ class ExampleService: Service() {
          *      START_REDELIVER_INTENT: The system will restart our service asap, and it will pass the last intent to it again
          *                              This played a bigger role for background services, because a foreground is very unlikely to be kill
          */
+
+        /**
+         * In order to stop the service from the inside when is the work is done
+         * we could call stopSelf() from inside the Service
+         */
+        stopSelf()
 
         return START_NOT_STICKY
     }
--- app/src/main/java/com/sample/foregroundserviceexample/MainActivity.kt
+++ app/src/main/java/com/sample/foregroundserviceexample/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.sample.foregroundserviceexample
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,8 +25,22 @@ class MainActivity : AppCompatActivity() {
         val serviceIntent = Intent(this, ExampleService::class.java)
         serviceIntent.putExtra(INPUT_EXTRA_KEY, input)
 
-        startService(serviceIntent)
+        // Start the Service while the App is still open
+//        startService(serviceIntent)
 
+        /**
+         * Start the Service while the App is on the background
+         * This method tells the system we want to promote our service to a foreground service as quickly as possible.
+         * After calling it we have a window of 5 seconds for calling startForeground from our Service class.
+         * If we don't do it in those 5 seconds, the system will kill our service immediately.
+         * If we call startService() from the background, it will throw an illegal state exception and our app will crash.
+         * There are some exceptions our app is on a temporary white-list,
+         * and it will be able to call startService() from the background without crash
+         *
+         * ContextCompat checks for us if our SDK is equal or bigger than Oreo,
+         * required since startForegroundService only works from that SDK version
+         */
+        ContextCompat.startForegroundService(this, serviceIntent)
     }
 
     fun stopService(view: View) {
